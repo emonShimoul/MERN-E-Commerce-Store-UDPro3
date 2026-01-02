134be38d7ec944f06f664b9abfe2f7630c82475e
emonShimoul <emonshimoul@gmail.com>
2026-01-02 22:08:24 +0600

    created and connected the routes successfully

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/signup", (req, res) => {
+    res.send("Sign up route called!!");
+});
+
+export default router;
+
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config(); // allow to read the content of dotenv file
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+// authentication
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
     console.log("Server is running on port http://localhost:" + PORT);
 });
